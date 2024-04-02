293f31b35b7071b986b0026d6150be4559d9f3f3
heclair <heclair@hotmail.com>
2024-04-01 21:28:07 -0300

    aula 01/04

---
+++ src/components/item/index.tsx
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export default function Item({children}:any){
+    return <Sld>{children}</Sld>;
+}
+
+const Sld = styled.p`
+    font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;`
+    
+    ;
